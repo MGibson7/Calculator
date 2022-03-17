0b583c6a1f16f3defd6749a4dfacb7d9d21795db
MGibson7 <MGibsonASC@gmail.com>
2022-03-17 15:59:01 -0400

    Problem with enter button clicking buttons again fixed by switching to equal button

--- calc.js
+++ calc.js
@@ -27,7 +27,7 @@ function operate(prevNum, operand, newNum){
     else if (operand == "-"){
         result = subtract(prevNum, newNum);
     }
-    else if (operand == "X" || operand =="*"){
+    else if (operand == "X"){
         result = multiply(prevNum, newNum);
     }
     else if (operand == "/" && newNum != 0){
@@ -75,6 +75,7 @@ let error = true
 
 buttons.forEach(element => {element.addEventListener('click', event =>{ 
     let number = (element.textContent)
+    console.log(number)
     if (result_display != true){
         let display = document.getElementById("display")
         content = display.textContent  
@@ -92,11 +93,13 @@ buttons.forEach(element => {element.addEventListener('click', event =>{
     operator = ""
     previousNum = previousNum + number
     result_display = false
+    return;
 })});
 
 //adding an event listener to non number buttons for function to display
 let nonNums = Array.from(document.getElementsByClassName("nonnum"))
 nonNums.forEach(element => {element.addEventListener('click', event =>{ 
+    console.log(operator)
     if (operator == ""){
         previousNum = parseInt(previousNum)
         operation.push(previousNum)
@@ -106,8 +109,10 @@ nonNums.forEach(element => {element.addEventListener('click', event =>{
         display.textContent = content + operator
         previousNum = 0
         operation.push(operator)
+        
 
     }
+    return;
 })});
 
 //EQUAL BUTTON
@@ -116,6 +121,7 @@ equal.addEventListener('click', event =>{
     previousNum = parseInt(previousNum)
     operation.push(previousNum)
     size = operation.length
+    console.log(operation)
     if (size >= 3 && operator ==""){
         result = operate(operation[0], operation[1], operation[2])
         opp = 1
@@ -147,6 +153,7 @@ equal.addEventListener('click', event =>{
         error = true
 
     }
+    return;
 })
 //CLEAR BUTTON
 let clear = document.getElementById("clear")
@@ -160,93 +167,36 @@ clear.addEventListener('click', event =>{
 
 })
 
-let header = document.getElementById("header")
-header.addEventListener('click', event =>{
-    console.log("clicked")
-})
 
 //keyboard
 opFunct = ['*', "+", "-", "/"]
 document.addEventListener('keydown', function (event) {
-    document.getElementById('header').click();
     numOrNo = isFinite(event.key)
     if (numOrNo === true) {
         let number = (event.key)
-        if (result_display != true){
-            let display = document.getElementById("display")
-            content = display.textContent  
-        }
-        else if (result_display == true && operator != "" && error !=true) {
-            let display = document.getElementById("display")
-            content = display.textContent
-        }
-        else{
-            content = ""
-            previousNum = 0
-        }
-        error = false
-        display.textContent = content + number
-        operator = ""
-        previousNum = previousNum + number
-        result_display = false
+        document.getElementById(`${event.key}`).click();
     }
     if (opFunct.includes(event.key)) {
-        if (operator == ""){
-            previousNum = parseInt(previousNum)
-            operation.push(previousNum)
-            operator = (event.key)
-            let display = document.getElementById("display")
-            let content = display.textContent
-            display.textContent = content + operator
-            previousNum = 0
-            operation.push(operator)
-    
+        if (event.key == "*"){
+            document.getElementById(`mult`).click();
         }
+        else if (event.key == "+"){
+            document.getElementById(`add`).click();
+        }
+        else if (event.key == "-"){
+            document.getElementById(`sub`).click();
+        }
+        else if (event.key == "/"){
+            document.getElementById(`div`).click();
+        }
+        
     }
-    if (event.key == "Enter"){
-        previousNum = parseInt(previousNum)
-        operation.push(previousNum)
-        console.log(operation)
-        size = operation.length
-        if (size >= 3 && operator ==""){
-            result = operate(operation[0], operation[1], operation[2])
-            opp = 1
-            opp2 = 2
-            if (size > 3){
-                size = size -3 
-                while (size > 0){
-                    opp = opp + 2
-                    opp2 = opp2 + 2
-                    size = size - 2
-                    result = operate(result, operation[opp], operation[opp2])
-                }
-
-            }
-            let display = document.getElementById("display")
-            display.textContent = result
-            operation = []
-            operator = ""
-            previousNum = result
-            result_display = true
-        }
-        else{
-            let display = document.getElementById("display")
-            display.textContent = "ERROR PLEASE TRY AGAIN"
-            operation = []
-            operator = 7
-            previousNum = ""
-            result_display = true
-            error = true
-
-        }
+    if (event.key == "="){
+        document.getElementById("equal").click();
     }
     if (event.key == "Backspace"){
-        let display = document.getElementById("display")
-        display.textContent = ""
-        operation = []
-        operator = 7
-        previousNum = ""
-        result_display = true
+        document.getElementById("clear").click();
+        
 
     }
   });
